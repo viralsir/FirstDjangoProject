05e1b27944955487cc49ce24336442608b053bd9
ViralSir <viralsir2018@gmail.com>
2024-08-30 18:53:43 +0530

    add flights models into ariline apps.

--- airline/models.py
+++ airline/models.py
@@ -8,7 +8,14 @@ class airport(models.Model):
     city = models.CharField(max_length=100)
     code = models.CharField(max_length=100)
 
+
     def __str__(self):
         return self.city + " - " + self.code
 
 
+class flight(models.Model):
+    departure = models.ForeignKey(airport, on_delete=models.CASCADE, related_name='flight_departure')
+    arrival = models.ForeignKey(airport, on_delete=models.CASCADE, related_name='flight_arrival')
+    duration = models.IntegerField(default=0)
+    def __str__(self):
+        return self.departure.city + " -> " + self.arrival.city
